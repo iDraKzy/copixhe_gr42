bc917cf09475ba89ba1338f5d7d9ffb56e27cb31
iDraKzy <57191896+iDraKzy@users.noreply.github.com>
2021-02-18 21:13:24 +0100

    spec for check_level, check_victory and create_map

---
+++ main.py
@@ -0,0 +1,54 @@
+def create_map(path):
+    """Create the data structure for the map and returns it.
+
+    Parameters
+    ----------
+    path: Path to the file containing the map information (str)
+
+    Returns
+    -------
+    main_structure: Main structure for the map (list)
+    ant_structure: List of existing ants (list)
+    anthill_structure: List of 2 elements containing the anthills information (list)
+
+    Versions
+    --------
+    specification: Youlan Collard (v.1 18/02/2021 )
+    
+    """
+
+def check_level(main_structure, anthill):
+    """Check the level of an anthill and returns it.
+
+    Parameters
+    ----------
+    main_structure: Main structure of the game, containing the map (list)
+    anthill: The anthill to be checked, from the anthill structure (dict)
+
+    Returns
+    -------
+    level: The level of the anthill (int)
+
+    Versions
+    --------
+    specification: Youlan Collard (v.1 18/02/2021)
+        
+    """
+
+def check_victory():
+    """Check if one of the player has win the game and returns the number of the team who has won.
+
+    Parameters
+    ----------
+    main_structure: Main structure of the game, containing the map (list)
+    anthill_structure: list containing the anthills information (list)
+
+    Returns
+    -------
+    won: Number of the team who has won, None if nobody has (int)
+
+    Versions
+    --------
+    specification: Youlan Collard (v.1 18/02/2021)
+    
+    """
