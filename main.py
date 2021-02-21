5bff79dce5a09fe1162e52fcabe0ee2f88ded7b0
F0xf1r3 <74051295+F0xf1r3@users.noreply.github.com>
2021-02-21 21:11:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def mourance(ant_pos, main_structure, ant_structure):
     specification: Martin Buchet (v.2 21/02/2021)
     """
     
-def main_fonction():
+def main_fonction(path):
     """
     call the others fonctions and count the turns
     
