c7a3b62fc52b4370c990454f7bbb6f7751103f73
Batte-Man <48249905+Batte-Man@users.noreply.github.com>
2021-02-22 16:40:23 +0100

    lifepoint on display spec

--- main.py
+++ main.py
@@ -313,9 +313,16 @@ def remove_ant_on_display(ant_position,carrying):
     spécification: Maxime Dufrasne  (v.1 22/02/21)
     """
 
-def update_lifepoint_on_display():
+def update_lifepoint_on_display(ant_id, ant_structure,):
     """Update the health bar of an ant on display
+    Parameter
+    ---------
+    ant_structure: Structure containing all the ants (list)
+    ant_id: Id of the desired ant (int)
     
+    Version
+    -------
+    specification: Martin Buchet (v.1 22/02/2021)
     """
 
 def lift_dirt_on_display():
